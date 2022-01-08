42a5f98b0849b1977332a6ac2c66dbd7603ce85f
Moiz Ahmed <moizahmed1996@hotmail.com>
2022-01-08 00:27:22 -0500

    Added function to add booksto the front end

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const fAuthor = document.querySelector("#form-author");
 const fPages = document.querySelector("#form-pages");
 
 let myLibrary = [];
+let dataValue = 1;
 
 function Book(title, author, pages) {
   this.title = title;
@@ -15,23 +16,36 @@ function Book(title, author, pages) {
 
 function addBookToLibrary() {
   form.addEventListener("submit", (e) => {
+    e.preventDefault();
     const newBook = new Book(fTitle.value, fAuthor.value, fPages.value);
     myLibrary.push(newBook);
+    bookSection.innerHTML = "";
+    displayBook();
     console.log(myLibrary);
-    e.preventDefault();
+    form.reset();
   });
 }
 
-const libraryDisplay = () => {
+const displayBook = () => {
   myLibrary.forEach((book) => {
+    const bookTitle = book.title;
+    const bookAuthor = book.author;
+    const bookPages = book.pages;
     const card = document.createElement("div");
+    card.dataset.bookIndex = dataValue;
+    dataValue++;
     card.classList.add("card");
     const h2 = document.createElement("h2");
-    h2.textContent = book.title;
+    h2.textContent = bookTitle;
     card.appendChild(h2);
+    const h3 = document.createElement("h3");
+    h3.textContent = bookAuthor;
+    card.appendChild(h3);
+    const h4 = document.createElement("h4");
+    h4.textContent = bookPages;
+    card.appendChild(h4);
     bookSection.appendChild(card);
   });
 };
 
 addBookToLibrary();
-libraryDisplay();
