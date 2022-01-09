b319d4c6f4f893ed5df5b59a8d5ee412977f66c8
Moiz Ahmed <moizahmed1996@hotmail.com>
2022-01-09 00:52:58 -0500

    Added functionality to delete individual cards

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const fAuthor = document.querySelector("#form-author");
 const fPages = document.querySelector("#form-pages");
 
 let myLibrary = [];
-let dataValue = 1;
+let dataValue = 0;
 
-function Book(title, author, pages) {
+function Book(title, author, pages, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.id = id;
 }
 
 function addBookToLibrary() {
@@ -27,13 +28,13 @@ function addBookToLibrary() {
 }
 
 const displayBook = () => {
-  myLibrary.forEach((book) => {
+  myLibrary.forEach((book, index) => {
     const bookTitle = book.title;
     const bookAuthor = book.author;
     const bookPages = book.pages;
+    book.id = "Book" + index;
     const card = document.createElement("div");
-    card.dataset.bookIndex = dataValue;
-    dataValue++;
+    // card.dataset.index = "Book" + index;
     card.classList.add("card");
     const h2 = document.createElement("h2");
     h2.textContent = bookTitle;
@@ -45,7 +46,24 @@ const displayBook = () => {
     h4.textContent = bookPages;
     card.appendChild(h4);
     bookSection.appendChild(card);
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    card.appendChild(button);
+    button.addEventListener("click", () => {
+      if (book.id) {
+        card.remove("[data-index" + book.id + "]");
+      }
+      myLibrary = myLibrary.filter((card) => {
+        return card.id !== book.id;
+      });
+    });
   });
 };
 
 addBookToLibrary();
+
+// let bookIndex = card.getAttribute("data-index");
+// console.log(bookIndex);
+// let removeBook = myLibrary.splice(bIndex, 1);
+// removeBook;
+// bookSection.remove("[data-index" + bookIndex + "]");
